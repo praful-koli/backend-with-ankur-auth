1dd06e1a4dba84953eb71b6cc74f5d79df0a3c6f
praful-koli <prafulkoli852@gmail.com>
2026-05-28 15:49:53 +0530

    feat(schema): user schema name eamil password implement

---
+++ notes/server/src/models/users.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+// User schema definition
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },  
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+// User model creation  
+const userModel = mongoose.model('user', userSchema)
+
+export default userModel
